ffe2e5a002e02676518cecad730a11096e103c45
jc <wnwlcks123@gmail.com>
2023-09-11 20:41:51 +0900

    컴포넌트 - 상태 관리(스토어)2

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import { Component } from "../core/heropy";
 import TextField from "../components/TextField";
 import Message from "../components/Message";
+import Title from "../components/Title";
 
 export default class Home extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class Home extends Component {
     `
     this.el.append(
       new TextField().el,
-      new Message().el)
+      new Message().el,
+      new Title().el)
   }
 }
