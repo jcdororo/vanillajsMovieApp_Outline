be65bd0e4641d88fe8b8a4cf9377a87df8f2c87a
jc <wnwlcks123@gmail.com>
2023-09-11 15:11:56 +0900

    컴포넌트 - 자식 컴포넌트에게 데이터 전달

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from './core/heropy.js'
+import FruitItem from './components/Fruititem.js'
 
 export default class App extends Component {
   constructor() {
@@ -14,15 +15,19 @@ export default class App extends Component {
   }
   render() {
     console.log(this.state.fruits)
-
     this.el.innerHTML = /* html */ `
       <h1>Fruits</h1>
-      <ul>
-        ${this.state.fruits
-          .filter(fruit => fruit.price < 3000)
-          .map(fruit => `<li>${fruit.name}</li>`)
-          .join('')}
-      </ul>
+      <ul></ul>
     `
+
+    const ulEl = this.el.querySelector('ul')
+    ulEl.append(...this.state.fruits
+      .map(fruit => new FruitItem({
+        props: {
+          name: fruit.name,
+          price: fruit.price
+        }
+      }).el)
+    )
   }
 }
